65514bbed9591cf3362bcb22722b2a6f61c755c2
s00d <Virus191288@gmail.com>
2019-10-09 18:38:10 +0500

    add simlink

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const fs = require('fs-extra');
 const path = require('path');
 const SshClient = require('./utils/ssh');
+const readline = require("readline-sync");
 
 isObject = function(a) {
   return (!!a) && (a.constructor === Object);
@@ -27,6 +28,7 @@ class FolderUploadWebpackPlugin {
         options.ignore = options.ignore ? options.ignore : null;
         options.ssh = options.ssh ? new options.ssh(options.logging, options.progress) : new SshClient(options.logging, options.progress);
         options.pathsClear = new RegExp(options.pathsClear ? options.pathsClear : '', 'gui');
+        options.confirmation = options.confirmation ? options.confirmation : false;
 
         this.options = options;
         this.upload = this.upload.bind(this);
@@ -83,44 +85,86 @@ class FolderUploadWebpackPlugin {
     async upload(compilation, callback) {
       const {paths, remotePath, clear, ssh, chmod, server} = this.options;
 
-      if(this.options.firstEmit) {
-        for(let i in server) {
-          let filesList = await this.walk(paths);
+      if(!this.options.confirmation || readline.keyInYN(chalk.bold.red("\nAre you sure you want to replace the server?"))) {
+        if(this.options.firstEmit) {
+          for(let i in server) {
+            let filesList = await this.walk(paths);
 
-          server[i].port = server[i].port || '22';
-          await ssh.connect(server[i]);
+            server[i].port = server[i].port || '22';
+            await ssh.connect(server[i]);
 
-          if (clear) {
-            this.log('Clearing remote folder '+formatRemotePath(remotePath)+'* ...', chalk.red);
-            await ssh.exec('rm -rf ' + formatRemotePath(remotePath)+ '*');
+            if (clear) {
+              this.log('Clearing remote folder '+formatRemotePath(remotePath)+'* ...', chalk.red);
+              await ssh.exec('rm -rf ' + formatRemotePath(remotePath)+ '*');
+            }
+
+            let dirList = {};
+            for(let i in filesList) {
+              try {
+                let dir = formatRemotePath(remotePath, filesList[i].remotePath);
+                if(dirList.hasOwnProperty(dir)) continue;
+                dirList[dir] = dir;
+
+                if(!await ssh.exists(dir)) {
+                  this.log('MAKE remote folder '+dir+' ...', chalk.green);
+                  await ssh.mkdir(dir, true).catch(() => null);
+                  await ssh.chmod(dir, chmod).catch(() => null);
+                }
+              } catch (e) {}
+            }
+
+            this.log('Uploading...', chalk.green);
+            await ssh.sendFile(filesList, remotePath);
+            this.log('end', chalk.green);
+            await ssh.end();
           }
-
-          let dirList = {};
-          for(let i in filesList) {
-            try {
-              let dir = formatRemotePath(remotePath, filesList[i].remotePath);
-              if(dirList.hasOwnProperty(dir)) continue;
-              dirList[dir] = dir;
-
-              if(!await ssh.exists(dir)) {
-                this.log('MAKE remote folder '+dir+' ...', chalk.green);
-                await ssh.mkdir(dir, true).catch(() => null);
-                await ssh.chmod(dir, chmod).catch(() => null);
-              }
-            } catch (e) {}
-          }
-
-          this.log('Uploading...', chalk.green);
-          await ssh.sendFile(filesList, remotePath);
-          this.log('end', chalk.green);
-          await ssh.end();
         }
-
       }
+
       this.options.firstEmit = false;
+
+      if(this.options.symlink) {
+        this.log('Making symlink...', chalk.green);
+        this.createSimlinks(this.options.symlink, compilation.options.output.path, clear)
+      }
 
       if (callback) {
         callback();
+      }
+
+    }
+
+    createSimlinks(options, inputPath, clear) {
+      if (options.force || fs.existsSync(inputPath)) {
+        const baseDir = process.cwd();
+        process.chdir(inputPath);
+        const origin = path.relative(path.dirname(options.path), inputPath);
+        this.log('Symlink path: ' + options.path, chalk.green);
+        try {
+          if (clear) {
+            if (fs.existsSync(options.path)) {
+              fs.removeSync(options.path);
+            }
+          }
+          fs.readlinkSync(options.path); // Raises if symlink doesn't exist
+          fs.unlinkSync(options.path);
+        } catch (e) {
+          // symlink doesn't exist
+        } finally {
+          fs.symlinkSync(origin, options.path);
+        }
+
+        process.chdir(baseDir);
+      }
+
+      if (clear) {
+        const parentPath = path.dirname(inputPath);
+        const ignore = path.basename(inputPath);
+        const dirs = fs.readdirSync(parentPath);
+        for(let i in dirs) {
+          if(!fs.statSync(path.join(parentPath, dirs[i])).isDirectory() || dirs[i] === ignore)  continue;
+          fs.removeSync(path.join(parentPath, dirs[i]));
+        }
       }
     }
 
